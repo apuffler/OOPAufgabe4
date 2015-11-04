ad993ee6388d35ba87d3af21dc8c68c3862a094a
JRyuzaki <zivanovicjovan@gmail.com>
2015-11-04 16:08:21 +0100

    Class created

---
+++ Brett.java
@@ -0,0 +1,9 @@
+public class Brett extends Schnittholz{
+	/**
+	 *@param (breite) Bezeichnet die Breite des Brettes. Es wird erwartet, dass breite > 8 gilt
+	 *@param (dicke)  Bezeichnet die Dicke des Brettes. Es wird erwartet, dass dicke > 0 und dicke < 4 gilt
+	 */
+	public Brett(double breite, double dicke){
+		super(breite, dicke);
+	}
+}
