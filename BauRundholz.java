2b4e95d10cb0f3827981c390fdff484ad4044417
JRyuzaki <zivanovicjovan@gmail.com>
2015-11-04 16:13:01 +0100

    Constructor done

--- BauRundholz.java
+++ BauRundholz.java
@@ -1,5 +1,5 @@
 public class BauRundholz extends Saegerundholz implements Bauholz{
 	public BauRundholz(int laenge, String datum, Etikett alt, double staerke){
-		
+		super(laenge, datum, alt, staerke);
 	}
 }
