397dc9ae5c1d1eb5639eebd6310392bb006713f4
root <root@localhost.localdomain>
2015-11-10 17:55:03 +0100

    Added Super

--- Kantholz.java
+++ Kantholz.java
@@ -1,10 +1,11 @@
-public class Kantholz extends Vollkantschnittholz implements Bauholz{
+public class Kantholz extends VollkantSchnittholz implements Bauholz{
 	
 	/**
 	 *@param (dicke) Für den Parameter dicke muss dicke >= 4 gelten
 	 *@param (breite) Für den Parameter breite muss breite < 3 * dicke gelten
 	 */
-	public Kantholz(int dicke, int breite){
+	public Kantholz(int laenge, String datum, Etikett alt, double breite, double dicke){
+		super(laenge, datum, alt, breite, dicke);
 
 	}
 }
