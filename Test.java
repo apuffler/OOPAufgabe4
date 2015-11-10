d34049a2e9e3b9402c9d8e1f31ecf66effdfa92a
root <root@localhost.localdomain>
2015-11-10 17:56:32 +0100

    Angefangen

--- Test.java
+++ Test.java
@@ -1,5 +1,11 @@
 public class Test{
 	public static void main(String[] args){
+		//Test 1
+		Energieholz eHolz1 = new Energieholz(100, "10-11-2015", null, 1000);
+		System.out.println("EnergierundHolz: ");
+		System.out.println("Datum: " + eHolz1.datum());
 		
+
+
 	}
 }
